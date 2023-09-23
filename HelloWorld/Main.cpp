30187c4e71d24c99217f69773a3f1c6093d521b3
Punarva <punarva.kondalkar2021@vitbhopal.ac.in>
2023-09-24 02:48:30 +0530

    Linking

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 
-void Log(const char* message);
+void Log(const char* message); //Linking
 int main()
 {
 	Log("Hello OP");
 	cout << "Hello World";
+	cin.get();
 }
